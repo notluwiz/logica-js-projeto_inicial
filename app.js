36ca7906aca45d223f2fc4b37904326c618ee84a
luwiz <le9851928@gmail.com>
2025-02-04 09:46:07 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número de 1 a 10.')
+
+if (chute == numeroSecreto) {
+    console.log ('Você acertou o núemro secreto!');
+}
