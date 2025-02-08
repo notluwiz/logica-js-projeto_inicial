e541af218dac188e4adf729e24f7d5f91f723194
luwiz <le9851928@gmail.com>
2025-02-08 09:51:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número de 1 a 10.');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao numerosecreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10')
+    chute = prompt('Escolha um número entre 1 e 10');
+
     // se chute for igual ao número secreto
+
     if (chute == numeroSecreto) {
-        alert(`Você acertou o núemro secreto! ${numeroSecreto}`);
+        alert(`Você acertou o núemro secreto ${numeroSecreto}! com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
-            alert(`O numero srecreto é menor que ${chute}`);
+            alert(`Seu numero secreto é menor que ${chute}`);
         } else {
             alert(`Seu numero secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
