47e8f649cd649741d88083c6347b47af965587c3
luwiz <le9851928@gmail.com>
2025-02-08 08:00:43 -0300

    atualizei o codigo

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número de 1 a 10.');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-     alert(`Você acertou o núemro secreto! ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O numero srecreto é menor que ${chute}`);
+//enquanto chute não for igual ao numerosecreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10')
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Você acertou o núemro secreto! ${numeroSecreto}`);
     } else {
-        alert(`Seu numero secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O numero srecreto é menor que ${chute}`);
+        } else {
+            alert(`Seu numero secreto é maior que ${chute}`);
+        }
     }
 }
+
 
